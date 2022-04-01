81b0025f8bf54d4c2c82c3a868ad00b52216995d
dolyprh <dolyprahoro@gmail.com>
2022-04-01 14:03:05 +0700

    create components spotifygetTrack

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import SpotifyAPI from './components/spotifyAPI';
+import SpotifyGetTrack from './components/getTrack/spotifyGetTrack';
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className='header'>
-        <h1 className="header-content">Playlist</h1>
-      </header>
-      <SpotifyAPI />
-    </div>
+        <SpotifyAPI />
+      </div>
   );
 }
 
--- src/components/spotifyAPI.js
+++ src/components/spotifyAPI.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react";
 import SpotifyGetPlaylist from "./getPlaylist/spotifyGetPlaylist";
-
+import SpotifyGetTrack from "./getTrack/spotifyGetTrack";
+import './style.css';
 export default function SpotifyAPI() {
-  const ClIENT_ID = "d3b87dbbd6b64ae5a80d0613bd182d5e"
+  const CLIENT_ID = "d3b87dbbd6b64ae5a80d0613bd182d5e"
   const REDIRECT_URI = "http://localhost:3000"
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-  const SPACE_DELIMITER = "%20";
-    const SCOPES = [
-        "user-read-currently-playing",
-        "user-read-playback-state",
-        "playlist-read-private",
-    ];
-    const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER);
+  const RESPONSE_TYPE = "token"
+  // const SPACE_DELIMITER = "%20";
+  // const SCOPES = [
+  //   "user-read-currently-playing",
+  //   "user-read-playback-state",
+  //   "playlist-read-private",
+  // ];
+  // const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER);
 
   const getReturned = (hash) => {
     const stringAfterHashtag = hash.substring(1);
@@ -40,13 +42,13 @@ export default function SpotifyAPI() {
   });
 
   const handleLogin = () => {
-      window.location = `${AUTH_ENDPOINT}?client_id=${ClIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`
+      window.location = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`
   }
 
   return (
     <div className='header'>
         <button onClick={handleLogin}>Login to spotify</button>
-        <SpotifyGetPlaylist />
+        <SpotifyGetTrack />
     </div>
   )
 }
