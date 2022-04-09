5b2529c9421ebffe60d9ede70432ecc52b5c8e9e
dolyprh <dolyprahoro@gmail.com>
2022-04-09 12:20:39 +0700

    router and route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import SpotifyPages from './pages/home';
+import SpotifyPages from './pages/home/home';
+import LoginApp from './pages/Login';
+import CreatePlaylist  from './pages/createPlaylist/';
+import { Redirect, BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { useSelector } from 'react-redux';
 import './App.css'
 
 function App() {
+  const { isAuthorized } = useSelector((state) => state.auth)
+
   return (
     <div className="App-header">
-      <header>
-        <h1>Sportify</h1>
-        <SpotifyPages />
-      </header>
+      <Router>
+        <Switch>
+          <Route path='/create-playlist' exact>
+              {isAuthorized ? <CreatePlaylist /> : <Redirect to="/" /> }
+          </Route>
+          <Route path="/" exact>
+            <LoginApp />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/createPlaylist/createPlaylist.js
@@ -0,0 +1,95 @@
+import { useState, useEffect } from "react";
+import SearchBar from "../../components/SearchBar/searchBar";
+import Playlist from "../../components/Playlist/playlist";
+import PlaylistItem from "../../components/CreatePlaylist/CreatePlaylist";
+import { login } from '../../redux/authSlice';
+
+function CreatePlaylist() {
+  const [tracks, setTracks] = useState([]);
+  const [isSearch, setIsSearch] = useState(false);
+  const [selectedTrackURI, setSelectedTrackURI] = useState([]);
+  const [selectedTracks, setSelectedTracks] = useState([]);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.hash);
+    const accessToken = params.get('#access_token');
+
+        if(accessToken !== null) {
+            
+            const getUser = async () => {
+                try{
+                    const response = await GetUserSpotify(accessToken);
+                    // setUser(response);
+                    dispatch(login({
+                        accessToken: accessToken,
+                        user: response
+                    }));
+                } catch(error) {
+                    console.log(error)
+                }
+            }; 
+            getUser();
+        }
+
+    }, [])
+
+    const filterSelectedTrack = () => {
+        return tracks.filter((track) => selectedTrackURI.includes(track.uri))
+    }
+
+    useEffect(() => {
+        if(!isSearch) {
+            const selectedTracks = filterSelectedTrack();
+            setTracks(selectedTracks);
+        }
+    }, [])
+    
+    const handleSearch = (searchTracks) => {
+        setIsSearch(true);
+        
+        const selectedSearchTracks = searchTracks.filter(
+            (track) => selectedTrackURI.includes(track.uri)
+        );
+
+        setTracks([...new Set([...selectedSearchTracks, ...searchTracks])]);
+    };
+    
+
+    const toggleSelect = (track) => {
+        const uri = track.uri;
+        if (selectedTrackURI.includes(uri)) {
+            setSelectedTrackURI(selectedTrackURI.filter((item) => item !== uri));
+            setSelectedTracks(selectedTrackURI.filter((item) => item.uri !== uri));
+        } else {
+            setSelectedTrackURI([...selectedTrackURI, uri]);
+            setSelectedTracks([...selectedTracks, track]);
+        }
+    };
+
+  return (
+    <div>
+        <div className="content-spotify">
+            <SearchBar onSuccess={(tracks) => handleSearch(tracks)} />
+            <PlaylistItem uris={selectedTrackURI} className="playlist-form"/>
+
+        </div>
+
+            {tracks.length === 0 && <p>No tracks</p>}
+
+            <div className="track-list">
+                {tracks.map((track) => (
+                    <Playlist
+                        key={track.id}
+                        url={track.album.images[2].url}
+                        title={track.name}
+                        artist={track.artists[0].name}
+                        album={track.album.name}
+                        toggleSelect={() => toggleSelect(track)}
+                    />
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default CreatePlaylist;
