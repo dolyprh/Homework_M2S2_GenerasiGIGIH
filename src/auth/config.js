8559bfd81c18a2695cfc9dd372d710863c4164be
dolyprh <dolyprahoro@gmail.com>
2022-04-23 23:11:03 +0700

    update uri

--- src/auth/config.js
+++ src/auth/config.js
@@ -1,6 +1,6 @@
 export const config = {
     CLIENT_ID : "d3b87dbbd6b64ae5a80d0613bd182d5e",
-    REDIRECT_URI : "http://localhost:3000",
+    REDIRECT_URI : "http://",
     AUTH_ENDPOINT : "https://api.spotify.com/v1",
     RESPONSE_TYPE : "token",
     SCOPES : "playlist-modify-private",
