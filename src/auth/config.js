f5337a46bbfa9174223556503bdc1326f021bdf6
dolyprh <dolyprahoro@gmail.com>
2022-04-23 23:19:02 +0700

    uri vercel

--- src/auth/config.js
+++ src/auth/config.js
@@ -1,6 +1,6 @@
 export const config = {
     CLIENT_ID : "d3b87dbbd6b64ae5a80d0613bd182d5e",
-    REDIRECT_URI : "http://",
+    REDIRECT_URI : "http://https://spotifyapps-generasigigih-homework.vercel.app/",
     AUTH_ENDPOINT : "https://api.spotify.com/v1",
     RESPONSE_TYPE : "token",
     SCOPES : "playlist-modify-private",
